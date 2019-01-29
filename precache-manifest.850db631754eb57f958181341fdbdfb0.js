a9f797d2f03b406077ea28e80938a6b4b2f2d9a8
John Jones <johnj@casefoundation.org>
2019-01-29 11:03:10 -0500

    Updates

---
+++ precache-manifest.850db631754eb57f958181341fdbdfb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba4694dd2ddcdbdd3854",
+    "url": "/logoizer/static/js/runtime~main.ba4694dd.js"
+  },
+  {
+    "revision": "afd0e9d04975e7bbb13d",
+    "url": "/logoizer/static/js/main.afd0e9d0.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/logoizer/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "afd0e9d04975e7bbb13d",
+    "url": "/logoizer/static/css/main.7e78500b.chunk.css"
+  },
+  {
+    "revision": "b28e1dd7bf11383028969118da922e9a",
+    "url": "/logoizer/index.html"
+  }
+];
